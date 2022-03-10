e5ce8f60c678b8c6b638619c589691abc46c26f1
jidesh85 <97509139+jidesh85@users.noreply.github.com>
2022-03-10 20:54:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "<h1><center>Docker Image Ansible </center><h1>"
+  return "<h1><center>Docker Image Ansible using Jenkins </center><h1>"
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',port=5000)
